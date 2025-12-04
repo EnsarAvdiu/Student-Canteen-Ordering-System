8cc6e6b72061c8b331b1c2c4325034f112d5b7c2
EnsarAvdiu <ensar.avdiu1@gmail.com>
2025-12-04 20:56:55 +0100

    ku me i gjujt pjest e tua

---
+++ projekti.js
@@ -0,0 +1,43 @@
+// Jona Gecaj Start
+
+
+
+// Jona Gecaj End
+
+
+
+
+
+
+
+
+//Jona Berbatovci Start
+
+
+//Jona Berbatovci End
+
+
+
+
+
+
+
+
+
+//Elion Sahiti Start
+
+
+
+//Elion Sahiti End
+
+
+
+
+
+
+
+//Ensar Avdiu Start
+
+
+
+//Ensar Avdiu End
